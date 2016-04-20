bf1a6c2f37673f5cdc77fdaf97ece61c37305756
D-Cipher <d.cipher.x@gmail.com>
2016-04-20 13:05:55 -0400

    4/20/16

--- Swift Courses/Complete IOS 9/Lectures/MyPlayground.playground/Contents.swift
+++ Swift Courses/Complete IOS 9/Lectures/MyPlayground.playground/Contents.swift
@@ -1,46 +1,15 @@
 import UIKit
 
-let board = ["", "", ""]
-
-func check_victory(board: AnyObject) {
+func validate(value: String) -> Bool {
     
-    //Possible Conditions:
-    var possibleConditions: Set = [
-        //Horizontals
-        [[0,0], [1,0], [2,0]],
-        [[0,1], [1,1], [2,1]],
-        [[0,2], [1,2], [2,2]],
-        //Verticals
-        [[0,0], [0,1], [0,2]],
-        [[1,0], [1,1], [1,2]],
-        [[2,0], [2,1], [2,2]],
-        //Diagonals
-        [[0,0], [1,1], [2,2]],
-        [[2,0], [1,1], [0,2]]
-    ]
+    let PHONE_REGEX = "^\\d{3}\\d{3}\\d{4}$"
     
-    var currentCondition = [[],[],[],[],[],[],[],[]]
-    var loopCount = -1
-    var slotNum = -1
-    var blanks = 0
+    var phoneTest = NSPredicate(format: "SELF MATCHES %@", PHONE_REGEX)
     
-    var test: [AnyObject] = []
+    var result =  phoneTest.evaluateWithObject(value)
     
-    //Find Combinations:
-    for combinations in possibleConditions {
-        for x in combinations {
-            loopCount += 1
-            if (loopCount)%3 == 0 {
-                slotNum += 1
-            }
-            currentCondition[slotNum].append(board[x])
-            
-        }
-        //letters = [board[y][x], for (x,y) in index]
-        
-    }
+    return result
     
 }
 
-
-check_victory(board)
+validate("3148073944")
