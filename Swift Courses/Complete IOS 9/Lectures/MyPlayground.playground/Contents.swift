f4f32c49f125be72bcf1472702a370b0183ef51d
D-Cipher <d.cipher.x@gmail.com>
2016-04-24 23:21:09 -0400

    4/24/16

--- Swift Courses/Complete IOS 9/Lectures/MyPlayground.playground/Contents.swift
+++ Swift Courses/Complete IOS 9/Lectures/MyPlayground.playground/Contents.swift
@@ -1,15 +1,32 @@
+import Foundation
 import UIKit
 
-let stripped_number = "12312312342"
-
-if stripped_number.characters.count == 10 {
-    let stringts: NSMutableString = NSMutableString(string: stripped_number)
-    stringts.insertString("-", atIndex: 3)
-    stringts.insertString("-", atIndex: 7)
-    let formatted_phoneNum = String(stringts)
-    print(formatted_phoneNum)
-} else {
-    let formatted_phoneNum = "number"
-    print(formatted_phoneNum)
+func test(firstName: String) -> String {
+    
+    let first_char = firstName.characters.first
+    var first_str = ""
+    
+    if first_char != nil {
+        first_str = String(first_char!)
+    } else {
+        first_str = ""
+    }
+    
+    let alphaNameTest = NSPredicate(format: "SELF MATCHES %@", "^([a-zA-Z])")
+    
+    let result = alphaNameTest.evaluateWithObject(first_str)
+    
+    guard result == true else {
+        let symbol_str = "#"
+        
+        return symbol_str
+    }
+    
+    let letter = firstName.characters.first
+    
+    let letter_str = String(letter!)
+    
+    return letter_str
 }
 
+test("Yan Yan")
